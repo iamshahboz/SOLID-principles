cb0038fbdfc913849438e02d42b04bc6c199d0cd
Shahboz <sharafzodashahboz@gmail.com>
2024-02-29 23:12:02 +0500

    the correct implementation of dip

---
+++ app_dip2.py
@@ -0,0 +1,18 @@
+# you should do 
+from abc import ABC, abstractmethod
+
+class FrontEnd:
+    def __init__(self, data_source):
+        self.data_source = data_source 
+
+    def display_data(self):
+        data = self.data_source.get_data()
+        print("Display data", data)
+
+class DataSource:
+    def get_data(self):
+        return "Data from the database"
+    
+class API(DataSource):
+    def get_data(self):
+        return "Data from API"
